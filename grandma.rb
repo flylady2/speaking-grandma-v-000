91ef3b50b4258674a899fa67bcbb4a3a276261a7
Leslie Stevens <flylady@mail.utexas.edu>
2018-04-10 20:39:24 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,6 +3,10 @@ def speak_to_grandma(phrase)
      return "I LOVE YOU TOO PUMPKIN!"
   else
     puts "HUH? SPEAK UP< SONNY!"
+  elsif phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
   end
 
 end
